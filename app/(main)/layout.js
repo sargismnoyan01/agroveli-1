4db51c884e796a1db592ac6ef5b17c3128fb5751
zhan-n <nikoghosyanjan10@gmail.com>
2026-01-18 01:17:45 +0400

    add meta data

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,33 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google"
 import { Header } from "@/components/layout/header";
 
-const _geist = Geist({ subsets: ["latin"] })
-const _geistMono = Geist_Mono({ subsets: ["latin"] })
-
-export const metadata = {
-  title: "AgroVeli — Сельхозтовары",
-  description:
-    "AgroVeli — платформа для поиска и размещения сельхозтоваров. Найдите агро-товары и свяжитесь с продавцом напрямую.",
-  generator: "v0.app",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
-}
-
 export const viewport = {
   themeColor: "#16a34a",
   width: "device-width",
--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,59 @@
-"use client"
 import './globals.css';
 import 'react-phone-number-input/style.css'
 import 'swiper/css';
 import "swiper/css/pagination";
 
 
+export const metadata = {
+  title: "AgroVeli — Сельхозтовары",
+  description:
+    "AgroVeli — платформа для поиска и размещения сельхозтоваров. Найдите агро-товары и свяжитесь с продавцом напрямую.",
 
-// export const metadata = {
-//   title: 'Agroveli',
-//   description: 'Agroveli',
-// };
+  icons: {
+    icon: [
+      {
+        url: "/assets/images/bg1.png",
+        media: "(prefers-color-scheme: light)",
+      },
+      {
+        url: "/assets/images/bg1.png",
+        media: "(prefers-color-scheme: dark)",
+      },
+      // {
+      //   url: "/icon.svg",
+      //   type: "image/svg+xml",
+      // },
+    ],
+    apple: "/assets/images/bg1.png",
+  },
+
+  openGraph: {
+    title: "Agroveli",
+    description: "Agroveli",
+    url: "https://agroveli.com",
+    siteName: "Agroveli",
+    images: [
+      {
+        url: "/assets/images/bg1.png",
+        width: 1200,
+        height: 630,
+        alt: "Agroveli",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Agroveli",
+    description: "Agroveli",
+    images: ["/assets/images/bg1.png"],
+  },
+};
 
 import { Roboto } from 'next/font/google';
-import { Provider } from "react-redux";
-import { store } from "@/lib/store";
+import Providers from "@/components/shared/Providers";
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -26,9 +66,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <body className={roboto.variable}>
-    <Provider store={store}>
+    <Providers >
       {children}
-    </Provider>
+    </Providers>
     </body>
     </html>
   );
